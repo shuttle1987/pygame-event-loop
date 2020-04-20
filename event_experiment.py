680c5c5cbb927a41684a0b72952d88fedb0ca1f7
Janis Lesinskis <janis@lesinskis.com>
2020-04-21 03:05:26 +1000

    Change text via thread

--- event_experiment.py
+++ event_experiment.py
@@ -1,3 +1,7 @@
+
+
+import threading
+
 import pygame
 import pygame.freetype
 
@@ -15,6 +19,15 @@ COLOR_WHITE = (255, 255, 255)
 screen_resolution = (1024, 768)
 screen = pygame.display.set_mode(size=screen_resolution)
 
+def text_changer():
+    while True:
+        new_text = input("New text:")
+        global text
+        text = new_text
+
+text_update_thread = threading.Thread(target=text_changer)
+text_update_thread.start()
+
 while True:
     screen.fill(COLOR_WHITE)
     for event in pygame.event.get():
@@ -29,8 +42,8 @@ while True:
                 font_coords[0] -= 10
             elif event.key == pygame.K_RIGHT:
                 font_coords[0] += 10
-        print(font_coords)
         FONT.render_to(surf=screen, dest=font_coords, text=text, fgcolor=(128, 255, 255))
         pygame.display.flip()
 
+text_update_thread.join()
 pygame.quit()
